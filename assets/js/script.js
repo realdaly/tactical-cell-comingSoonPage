a52175ee91c7b2b9682df8bf798add097970ae5b
realdaly <mohammed96218@yahoo.com>
2022-11-29 07:40:17 +0300

    added rotating ring around the logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,3 @@
-// Logo Follow Cursor Effect
-const imgContainer = document.querySelector(".container")
-const img = document.querySelector(".img img")
-document.addEventListener("mousemove", ({ clientX, clientY }) => {
-    const { top: topIndent, right: rightIndent, width, height } = imgContainer.getBoundingClientRect()
-    let xAxis = -((clientX - (rightIndent * 0.01)) - width / 2) / 50
-    let yAxis = ((clientY - (topIndent * 0.01)) - height / 2) / 50
-    img.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg) rotateZ(${yAxis}deg)`
-})
-
-// Animate In
-document.addEventListener("mouseenter", (e) => {
-    img.style.transform = "translateZ(50vw)"
-})
-
-//Animate Out
-document.addEventListener("mouseleave", (e) => {
-    img.style.transform = "translateZ(0)"
-})
-
-
 // Text Erase Effect
 const animateText = document.querySelector(".animateText")
 const textWrapper = document.querySelector(".textWrapper")
